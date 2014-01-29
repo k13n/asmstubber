cb824364dd865a37969a5afdf12d493bd570300a
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-01-29 17:02:44 +0100

    Initial commit, create directory structure and pom.xml

---
+++ src/main/java/org/k13n/swtstubber/App.java
@@ -0,0 +1,13 @@
+package org.k13n.swtstubber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
