e82af7705ca7ea7ca871546bfe0a3bf75113f0e1
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-02-01 11:58:28 +0100

    Wire things together in the App class

--- src/main/java/org/k13n/swtstubber/App.java
+++ src/main/java/org/k13n/swtstubber/App.java
@@ -1,13 +1,29 @@
 package org.k13n.swtstubber;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Set;
+
+import org.k13n.swtstubber.indexing.ClassIndex;
+import org.k13n.swtstubber.indexing.ClassIndexer;
+import org.k13n.swtstubber.matcher.SwtMatcher;
+import org.k13n.swtstubber.visitors.ClassExplorer;
+
+public class App {
+
+  public static void main(String[] args) {
+    ClassIndex index = new ClassIndex();
+    ClassIndexer indexer = new ClassIndexer(index);
+
+    indexer.index("/path/to/eclim/build");
+    Set<String> eclimClasses = index.keys();
+
+    indexer.index("/path/to/eclipse/plugins");
+    System.out.printf("Indexed classes: %d%n", index.keys().size());
+
+    SwtMatcher swtMatcher = new SwtMatcher();
+    ClassExplorer explorer = new ClassExplorer(index, swtMatcher, eclimClasses);
+
+    explorer.explore();
+    swtMatcher.dump();
+  }
+
 }
