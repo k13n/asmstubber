3fc5b21b1970acecd54afc11122775ec668aa93b
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-02-07 10:32:41 +0100

    Add methods to write the transformed bytecode to a file

--- src/main/java/org/k13n/swtstubber/util/FileUtil.java
+++ src/main/java/org/k13n/swtstubber/util/FileUtil.java
@@ -1,6 +1,8 @@
 package org.k13n.swtstubber.util;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -14,6 +16,45 @@ public class FileUtil {
     while ((length = stream.read(buffer)) != -1)
       baos.write(buffer, 0, length);
     return baos.toByteArray();
+  }
+
+  public static void writeBytecode(String targetDirectory, String className,
+      byte[] bytecode) throws IOException {
+    String packageName = packageName(className);
+    createPackageHierarchy(targetDirectory, packageName);
+    String file = targetDirectory + File.separator + className + ".class";
+    FileOutputStream stream = new FileOutputStream(new File(file));
+    stream.write(bytecode, 0, bytecode.length);
+    stream.flush();
+    stream.close();
+  }
+
+  private static String createPackageHierarchy(String targetDirectory,
+      String packageName) {
+    String path = targetDirectory;
+    for (String folder : packageName.split("\\.")) {
+      path = path + File.separator + folder;
+      createDirectoryIfNotExists(path);
+    }
+    return path;
+  }
+
+  private static void createDirectoryIfNotExists(String directory) {
+    File file = new File(directory);
+    if (file.exists()) {
+      if (!file.isDirectory()) {
+        String msg = "Path " + directory + " exists, but is not a directory";
+        throw new RuntimeException(msg);
+      }
+    } else {
+      file.mkdir();
+    }
+  }
+
+  private static String packageName(String internalName) {
+    int separatorPos = internalName.lastIndexOf("/");
+    String packageName = internalName.substring(0, separatorPos);
+    return packageName.replace('/', '.');
   }
 
 }
